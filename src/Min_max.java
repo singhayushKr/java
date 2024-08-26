eac9c948fe85f4e2b4d62d115ccfdb6efe5b015a
Ayush kr Singh <ayushsingh4167@gmail.com>
2024-08-26 18:28:41 +0530

    first commit

--- src/Min_max.java
+++ src/Min_max.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Min_max {
     public static void main(String[] args) {
+        Scanner s =new Scanner(System.in);
+        System.out.println("Enter three numbers with space separated");
+        int x=s.nextInt();
+        int y=s.nextInt();
+        int z=s.nextInt();
+        int min_of_three=minimum(x,y,z);
+        int max_of_three=maximum(x,y,z);
+        System.out.println("minimun is "+min_of_three);
+        System.out.println("maximum is "+max_of_three);
 
+    }
+    static int maximum(int a, int b, int c) {
+        return Math.max(c,Math.max(a,b));
+    }
+
+    static int minimum(int a,int b,int c){
+        return Math.min(c,Math.min(a,b));
     }
 }
